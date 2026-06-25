ef6785e8e7aa4ce4090ded7e1a4082c4c1ae5132
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-06-25 04:52:32 -0300

    exercício 1

---
+++ alimentacao.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int n, m, total_alunos = 0, maior = 0, i, j, **frutas;
+
+    scanf("%d %d", &n, &m);
+
+    frutas = malloc(n*sizeof(int*));
+
+    for (i = 0; i < n; i++) {
+        frutas[i] = malloc(m*sizeof(int));
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            scanf("%d", &frutas[i][j]);
+        }
+    }
+
+    for (j = 0; j < m; j++) {
+        maior = 0;
+
+        for (i = 0; i < n; i++) {
+            if (frutas[i][j] > maior) {
+                maior = frutas[i][j];
+            }
+        }
+
+        total_alunos += maior;
+    }
+
+    printf("%d", total_alunos);
+
+    for (i = 0; i < n; i++) {
+        free(frutas[i]);
+    }
+
+    free(frutas);
+
+    return 0;
+}
